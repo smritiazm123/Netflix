cf91a1da5c41ca4489b066c8093232d8c05b206c
unknown <smritiazm123@gmail.com>
2024-12-28 22:31:56 +0530

    signin and signup for(login page)

--- src/components/login.js
+++ src/components/login.js
@@ -1,13 +1,53 @@
-import React from 'react'
-import Header from './Header';
+import React, { useState } from "react";
+import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  function toogleSignInForm() {
+    setIsSignInForm(!isSignInForm);
+  }
   return (
     <div>
-        <Header/>
-        <img src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_small.jpg" alt="background"/>
+      <Header />
+      <div className="absolute ">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/aa9edac4-a0e6-4f12-896e-32c518daec62/web/IN-en-20241223-TRIFECTA-perspective_1502c512-be5f-4f14-b21a-e3d75fe159ab_small.jpg"
+          alt="background"
+        />
+      </div>
+      <form className="absolute p-12 bg-black w-3/12 mx-auto right-0 left-0 my-36 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-2 w-full bg-gray-700"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-2 w-full bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-2 w-full bg-gray-700"
+        />
+        <button className="p-4 my-4 bg-red-700 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toogleSignInForm}>
+          {" "}
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now"}
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
